4a5bb7f22ac9571b0f51e931690677df82c1725f
jdawg <jake@nokware.net>
2025-01-02 16:02:39 -0700

    finalizing TinaCMS migration and tidying up file structures

---
+++ scsmission.org/tina/config.ts
@@ -0,0 +1,211 @@
+import { defineConfig } from "tinacms";
+
+import { authorFields } from "./templates";
+import { eventFields } from "./templates";
+import { kirtanFields } from "./templates";
+import { new_pageFields } from "./templates";
+import { noticeFields } from "./templates";
+import { postFields } from "./templates";
+import { researchFields } from "./templates";
+import { scholarshipFields } from "./templates";
+import { teacherFields } from "./templates";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Post",
+        name: "post",
+        path: "content/blog",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Author",
+        name: "author",
+        path: "content/author",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...authorFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Kirtan",
+        name: "kirtan",
+        path: "content/kirtan",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...kirtanFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Teacher",
+        name: "teacher",
+        path: "content/teacher",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...teacherFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Event",
+        name: "event",
+        path: "content/event",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...eventFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "About",
+        name: "about",
+        path: "content/about",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+          },
+        },
+      },
+      {
+        format: "md",
+        label: "Contact",
+        name: "contact",
+        path: "content/contact",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+          },
+        },
+      },
+      {
+        format: "toml",
+        label: "Configuration",
+        name: "configuration",
+        path: "scsmission.org",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
